1a0b6be36f5ee0f60572194026653ae85a0ed262
relbarto <roberto.restrepo@winsiper.com>
2019-08-30 15:52:33 -0500

    cambios pendientes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+
+const port = process.env.PORT || 3000;
+
+
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -31,6 +36,6 @@ mongoose.connect('mongodb://localhost:27017/task',
   });
 
 
-app.listen(3000, () => {
-  console.log('escuchando peticiones express en el puerto', 3000);
+app.listen(port, () => {
+  console.log(`escuchando peticiones express en el puerto ${port}`);
 });
